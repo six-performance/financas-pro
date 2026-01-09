51894e00ef271802c7ef62c10a1902ef48367c08
Carlos Santos <carlos_santos0702@hotmail.com>
2026-01-08 21:09:10 -0300

    Configurações inciais do projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>Hello World</h1>
+      <Button/>
     </div>
   );
 }
