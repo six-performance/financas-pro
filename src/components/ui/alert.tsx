8adb60d7ac5f37e5fef147a15872ea039067a082
Diêgo Claudiano <dgsilva.230493@gmail.com>
2026-01-20 21:18:35 -0300

    refatoração tela login

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -11,6 +11,8 @@ const alertVariants = cva(
         default: "bg-card text-card-foreground",
         destructive:
           "text-destructive bg-card [&>svg]:text-current *:data-[slot=alert-description]:text-destructive/90",
+        success:
+          "border-green-500 bg-green-50 text-green-700 [&>svg]:text-green-600 *:data-[slot=alert-description]:text-green-700/90",
       },
     },
     defaultVariants: {
